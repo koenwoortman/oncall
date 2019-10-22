fbdcec1121bf14dc6cd2702bc6ba19a1d0b25781
Koen Woortman <k.woortman@m2mobi.com>
2019-10-22 13:35:47 +0200

    Add help info

--- lib/oncall/cli.rb
+++ lib/oncall/cli.rb
@@ -1,16 +1,51 @@
 # frozen_string_literal: true
 require 'net/http'
 require 'yaml'
+require 'optparse'
 
 module Oncall
   module CLI
-    def self.invoke
+    extend self
 
+    USAGE = <<-EOF
+Usage: oncall
+
+Options:
+  --help      Display this help message
+    EOF
+
+    def invoke
+      options = parse_options
+
+      if options[:help]
+        print_usage
+        exit 0
+      end
+
+      run_tests
+    end
+
+    private
+
+    def parse_options
+      options = {}
+      OptionParser.new do |opt|
+        opt.on('--help') { |o| options[:help] = o }
+      end.parse!
+
+      options
+    end
+
+    def print_usage
+      puts USAGE
+    end
+
+    def run_tests
       begin
         config = YAML.load_file('oncall.yml')
       rescue
         puts 'Cannot load oncall.yml'
-        exit(1)
+        exit 1
       end
 
       path = ARGV[0]
