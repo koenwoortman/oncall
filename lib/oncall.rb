5db32df04abdfb140dd542e293a18397e14257ab
Koen Woortman <k.woortman@m2mobi.com>
2019-10-25 10:43:27 +0200

    Parse headers set in tests

--- lib/oncall.rb
+++ lib/oncall.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'net/http'
+require 'uri'
 require 'yaml'
 require 'optparse'
 require 'json-schema'
--- lib/oncall/request.rb
+++ lib/oncall/request.rb
@@ -2,22 +2,37 @@ module Oncall
   class Request
     def initialize(endpoint)
       @endpoint = endpoint
+      @headers = {}
     end
 
     def bootstrap(&block)
       instance_eval(&block)      
     end
 
-    def run; end
+    def run
+      uri = URI.parse("http://localhost:4567#{@endpoint}")
+      http = Net::HTTP.new(uri.host, uri.port)
+
+      request = Net::HTTP::Get.new(uri.request_uri)
+
+      request['User-Agent'] = 'Oncall'
+
+      @headers.each do |key, value|
+        request[key] = value
+      end
+
+      response = http.request(request)
+      puts response
+    end
 
     private
 
     def header(header)
-      puts header
+      @headers.merge!(header)
     end
 
     def body(description, &block)
-      puts description 
+      puts description
     end
 
     def status(description, &block)
