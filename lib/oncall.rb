34b69ff485054e79f30ce2854fd82f2f62c5d17b
Koen Woortman <k.woortman@m2mobi.com>
2019-10-31 10:37:33 +0100

    Remove unused code

--- lib/oncall.rb
+++ lib/oncall.rb
@@ -7,10 +7,5 @@ require 'json-schema'
 
 # Oncall
 module Oncall
-  require File.expand_path(File.join(File.dirname(__FILE__), 'oncall', 'body_test'))
   require File.expand_path(File.join(File.dirname(__FILE__), 'oncall', 'cli'))
-  require File.expand_path(File.join(File.dirname(__FILE__), 'oncall', 'request'))
-  require File.expand_path(File.join(File.dirname(__FILE__), 'oncall', 'test_case'))
-  require File.expand_path(File.join(File.dirname(__FILE__), 'oncall', 'test_wrapper'))
-  require File.expand_path(File.join(File.dirname(__FILE__), 'oncall', 'runner'))
 end
--- lib/oncall/commands/run_command.rb
+++ lib/oncall/commands/run_command.rb
@@ -4,9 +4,6 @@ module Oncall
   module Commands
     class RunCommand
       def self.invoke(args)
-        # env = ARGV[0]
-        # runner = Oncall::Runner.new(env)
-        # exit runner.run.to_i
         suite = Oncall::Suite.new
         suite.collect
         suite.process
