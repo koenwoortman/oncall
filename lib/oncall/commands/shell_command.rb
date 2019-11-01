e26a8d1ea830d4dfe31e94d2f92bbea1ac5ea34a
Koen Woortman <k.woortman@m2mobi.com>
2019-11-01 15:28:44 +0100

    Add in a repl example for shell command

--- lib/oncall/commands/shell_command.rb
+++ lib/oncall/commands/shell_command.rb
@@ -1,7 +1,16 @@
+require 'readline'
+
 module Oncall
   module Commands
     class ShellCommand
       def self.invoke(args)
+        while input = Readline.readline("> ", true)
+          break     if input == "exit"
+
+          # Remove blank lines from history
+          Readline::HISTORY.pop if input == ""
+          system(input)
+        end
       end
     end
   end
