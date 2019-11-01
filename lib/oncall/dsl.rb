3ff34908c7b5af46ad7d86547a1db59daa6b0e4c
Koen Woortman <k.woortman@m2mobi.com>
2019-11-01 15:34:28 +0100

    Remove old DSL code

--- lib/oncall/dsl.rb
+++ lib/oncall/dsl.rb
@@ -1,32 +1,4 @@
-require_relative 'dsl/response'
-
 module Oncall
-  class DSL
-    private
-
-    def describe(group, &block)
-      if block_given?
-        begin
-          yield
-        end
-      else
-        # Return status empty
-      end
-    end
-
-    def get(path, &block)
-      if block_given?
-        http = Net::HTTP.new('localhost', 4567)
-        request = Net::HTTP::Get.new(path)
-        response = Oncall::Response.new(http.request(request))
-
-        begin
-          resuls = response.instance_eval &block
-          resuls
-        end
-      else
-        # Return status empty
-      end
-    end
+  module DSL
   end
 end
