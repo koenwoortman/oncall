a09dcc717f4e9af5c8ef6a2dd8c003c591f58da7
Koen Woortman <k.woortman@m2mobi.com>
2019-11-07 13:56:49 +0100

    Implement the init again

--- lib/oncall/invocations.rb
+++ lib/oncall/invocations.rb
@@ -17,7 +17,14 @@ module Oncall
     end
 
     class InitRunner
-      def run(err, out); end
+      def run(err, out)
+        config_template_path = File.join(File.dirname(__FILE__), 'templates', 'oncall.yml.template')
+        template = File.read(config_template_path)
+
+        File.open(File.join(Dir.getwd, 'oncall.yml'), 'w') do |file|
+          file.write(template)
+        end
+      end
     end
 
     class TestRunner
